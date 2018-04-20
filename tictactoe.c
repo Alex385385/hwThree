df664340c7f3f86fa890800dd4b2acf719476c88
Alejandro Millan <alejandro_millan@ymail.com>
2018-04-19 18:46:01 -0700

    update 5

--- tictactoe.c
+++ tictactoe.c
@@ -2,7 +2,34 @@
  * tictactoe.c
  *
  *  Created on: Apr 19, 2018
- *      Author: amillan1
+ *      Author: Alejandro Millan
  */
+#include <stdio.h>
 
+const int size = 3;
+char ticTac[3][3] = {{'*','*','*'},{'*','*','*'},{'*','*','*'}};
 
+char b = '0';
+
+void draw()
+{
+   for(int i = 0; i < size; i++)
+   {
+      for(int j = 0; j < size; j++)
+      {
+         printf("%d" ticTac[i][j]);
+      }
+      printf("\n");
+   }
+}
+
+bool player1()
+{
+   printf("Enter your move");
+   scanf()
+}
+
+int main()
+{
+
+}
